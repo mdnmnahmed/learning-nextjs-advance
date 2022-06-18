4d576bf02bad012f0223236ba9638c487f2e3422
Numan Ahmed <mdnmnahmed@gmail.com>
2022-06-18 17:57:28 +0530

    Basic layout & navigation setup

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -4,30 +4,40 @@ import React from 'react';
 const Navigation = ({ navColor }) => {
     return (
         <>
-            <style>
-                {`
-                    a{
-                        display: 'inline-block';
-                        color: ${navColor};
-                        margin: 10px 20px;
-                        font-weight: bolder;
-                    }
-                `}
-            </style>
+            <header class="text-gray-400 body-font" style={{}}>
+                <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                    <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+                        </svg>
+                        <span class="ml-3 text-xl">N-News</span>
+                    </a>
+                    <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+                        <Link href={`/`}>
+                            <a className="mr-5 hover:text-white">Home</a>
+                        </Link>
 
-            <div>
-                <Link href={`/`}>
-                    <a>Home</a>
-                </Link>
+                        <Link href={`/user`}>
+                            <a className="mr-5 hover:text-white">User</a>
+                        </Link>
 
-                <Link href={`/user`}>
-                    <a>user</a>
-                </Link>
+                        <Link href={`/post`}>
+                            <a className="mr-5 hover:text-white">Posts</a>
+                        </Link>
 
-                <Link href={`/post`}>
-                    <a>post</a>
-                </Link>
-            </div>
+                        <Link href={`/about`}>
+                            <a className="mr-5 hover:text-white">About</a>
+                        </Link>
+                    </nav>
+                    <Link href={`/login`}>
+                        <button class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+                            Login
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                                <path d="M5 12h14M12 5l7 7-7 7"></path>
+                            </svg>
+                        </button>
+                    </Link>
+                </div>
+            </header>
         </>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
-import Head from 'next/head'
-import Navigation from '../components/Navigation'
-
 export default function Home() {
     return (
         <>
             <div>
                 <h1>Home</h1>
             </div>
-            <section>
 
-            </section>
+
         </>
     )
 }
