145d20eea69f875ed78f02e87496b6a7ed06d19f
Numan Ahmed <mdnmnahmed@gmail.com>
2022-06-28 21:43:34 +0530

    saving into DB

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -1,19 +1,26 @@
 import { dbConnect } from "../../../lib/dbConnect";
+import UserModal from "../../../modals/UserModal";
+import { errorHandler } from "../../../utils/errorHandler";
+import { responseHandler } from "../../../utils/responseHandler";
+import { validateInputFields } from "../../../utils/validateInputFields";
 
 export default async function handler(req, res) {
-    if (req.method !== 'GET') {
-        res.status(404).json({ message: 'Invalid Request' });
+    if (req.method !== 'POST') {
+        errorHandler('Invalid Request', res);
         return;
     }
 
-    await dbConnect();
+    try {
+        const { name, email, password } = req.body;
+        validateInputFields({ name, email, password });
 
-    const { name, email, tech } = req.body;
-
-    res.status(200).json({
-        message: 'Data Sanitized',
-        name,
-        email,
-        tech
-    });
+        await dbConnect();
+        const user = new UserModal(req.body);
+        const savedUser = await user.save();
+        if (savedUser) {
+            responseHandler(savedUser, res, 'Signup Success');
+        }
+    } catch (error) {
+        errorHandler(error, res, 'Failed to Signup, please try again');
+    }
 } 
