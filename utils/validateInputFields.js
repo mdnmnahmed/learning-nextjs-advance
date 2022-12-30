c354e00b91d86ba598ae5f549c0a9ed44aa6461b
Numan Ahmed <mdnmnahmed@gmail.com>
2022-12-31 01:43:07 +0530

    bug fix in fields input validation

--- utils/validateInputFields.js
+++ utils/validateInputFields.js
@@ -1,6 +1,6 @@
 export const validateInputFields = (fields) => {
     for (let key in fields) {
-        if (fields[key].trim() === "") {
+        if (!fields[key] || fields[key].trim() === "") {
             throw `${key} is Required, please fill ${key}.`;
         }
     }
